6e8eab4d4219556867c1f86bba9e9244a10b04d3
panda <1227781351@qq.com>
2022-10-27 14:30:33 +0800

    develop第二次提交

--- ware-center/src/main/java/com/base/warecenter/controller/forpublic/WareController.java
+++ ware-center/src/main/java/com/base/warecenter/controller/forpublic/WareController.java
@@ -1,7 +1,13 @@
 package com.base.warecenter.controller.forpublic;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WareController {
+    @RequestMapping(value = "get",method = RequestMethod.GET)
+    public String get(){
+        return "get something";
+    }
 }
